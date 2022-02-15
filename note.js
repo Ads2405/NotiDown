b9e09968729e1d41a2f92bb8db22a51ed988fa6a
Anmol Deshratna Saxena <92632502+LegendMortal07@users.noreply.github.com>
2022-02-15 22:19:38 +0530

    Add files via upload

---
+++ note.js
@@ -0,0 +1,70 @@
+console.log("this is a NOTE TAKING APP ");
+
+// creating button functs
+let addBtn = document.getElementById("addBtn");
+addBtn.addEventListener("click", function (e) {
+  let addTxt = document.getElementById("addTxt");
+  let addTitle = document.getElementById("addTitle");
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  let myObj = {
+    title: addTitle.value,
+    text: addTxt.value
+  }
+  notesObj.push(myObj);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  addTxt.value = "";
+  addTitle.value = "";
+
+  displayNotes();
+});
+
+displayNotes = () => {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  let htmladd = "";
+  notesObj.forEach((element, index) => {
+    htmladd += `
+            <div class="noteCard my-3 mx-3 card" style="width: 15rem; height: 10rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">${element.title}</h5>
+                        <p class="card-text"> ${element.text}</p>
+                        <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
+                    </div>
+                </div>`;
+  });
+  let noteAdd = document.getElementById("notes");
+
+  if (notesObj.length != 0) {
+    noteAdd.innerHTML = htmladd;
+  } 
+  else {
+
+    noteAdd.innerHTML = "please type something!!";
+  }
+};
+// Delete the Note
+function deleteNote(index) {
+
+
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+
+  notesObj.splice(index, 1);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  displayNotes();
+}
+
+
